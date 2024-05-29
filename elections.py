4797bebb4e1d82a3dc67edf6de0b8654762a1805
nielsdevos19 <142342513+nielsdevos19@users.noreply.github.com>
2024-05-29 18:26:48 +0200

    Update elections.py

--- elections.py
+++ elections.py
@@ -1,8 +1,5 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Output, Input
-import dash_bootstrap_components as dbc
+from dash import dcc, html, Output, Input, dbc
+
 import pandas as pd
 import plotly.express as px
 
